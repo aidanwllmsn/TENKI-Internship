870b5739b464581cc6c4c0844fc3e5f7834fd2a1
aidanwllmsn <ajwill1022@gmail.com>
2024-07-10 17:00:43 +0900

    Big fixes and more loading features

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
   const [userMessage, setUserMessage] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [loadingText, setLoadingText] = useState('Analyzing');
+  const [loadingTextUpdate, setLoadingTextUpdate] = useState('This may take a while.')
 
   useEffect(() => { // Analyzing dot animation
     if (isLoading) {
@@ -29,6 +30,8 @@ export default function Home() {
     'Generate an option 3 in this same exact format as the previous 2 that uses more concise keywords that aligns with the strategic objective of maximizing search visibility on Rakuten. This includes a final check for completeness, relevance, and adherence to Rakuten’s SEO best practices.',
     'From the 3 options you have just provided me, can you give me a score on each option on how well they would perform on Rakuten based off maximizing search visibility. Can you format as simplistic as possible with minimal explanation like so: Option 1: score Option 2: score Option 3: score'
   ];
+
+  // Counter keeps track of which query is being executed
   let counter = -1;
 
   const sendMessage = async (message) => { 
@@ -60,14 +63,20 @@ export default function Home() {
           const firstParagraph = accumulatedData.split('\n\n')[0];
           setChatHistory((prevChatHistory) => {
             const newChatHistory = [...prevChatHistory];
-            // console.log(counter); for testing
+            
+            // Detect which query is being ran
             if (counter === 2) {
+              setLoadingTextUpdate("Generating score.")
               counter += 1;
               newChatHistory.push({ role: "options", content: firstParagraph });
             } else if (counter === 3) {
               setIsLoading(false);
               newChatHistory.push({ role: "score", content: firstParagraph });
+            } else if (counter === 1) {
+              setLoadingTextUpdate('Generating option 3')
+              newChatHistory.push({ role: "options", content: firstParagraph });
             } else {
+              setLoadingTextUpdate('Generating option 2')
               newChatHistory.push({ role: "options", content: firstParagraph });
             }
             
@@ -98,16 +107,22 @@ export default function Home() {
     await fetch("/api/generate?endpoint=reset", { method: "POST" });
   };
 
+  // Regenerate last submitted message
   const regenerate = async () => {
-    setIsLoading(true);
-    sendMessage(userMessage);
+    if (userMessage.length != 0) {
+      setIsLoading(true);
+      setLoadingTextUpdate('This may take a while.')
+      sendMessage(userMessage);
+    } 
   };
 
+  // Events when submit is clicked
   const onSubmit = (event) => {
     event.preventDefault();
     if (!message.trim()) return;
     counter = -1
     setIsLoading(true);
+    setLoadingTextUpdate('This may take a while.')
     setUserMessage(message.trim());
     sendMessage(message.trim());
     setMessage("");
@@ -121,7 +136,7 @@ export default function Home() {
       <h1 className={styles.heading1}>TENKI-JAPAN Keyword Optimizer</h1>
       {!isLoading && chatHistory.length == 0 && <p className={styles.loadingTextsmall}>Submit a listing. Three optimized options will display here.</p>}
       {isLoading && <p className={styles.loadingText}>{loadingText}</p>}
-      {isLoading && <p className={styles.loadingTextsmall}>This may take a while</p>}
+      {isLoading && <p className={styles.loadingTextsmall}>{loadingTextUpdate}</p>}
       <div className={styles.chatContainer}>
       {!isLoading && chatHistory.map((msg, index) => (
           <div
